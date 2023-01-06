3f16aa1a0f48c8b4ab265104c28753dff1d0eb26
Khaled <khalegjr@gmail.com>
2023-01-06 16:50:58 -0300

    Update App component
    - Renders header and footer components
    - Test tags for input and chart components

--- challenge-solution/src/App.tsx
+++ challenge-solution/src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <div>
+        <h3>Input Area</h3>
+      </div>
+
+      <div>
+        <h3>Chart Area</h3>
+      </div>
+
+      <Footer />
     </div>
   );
 }
