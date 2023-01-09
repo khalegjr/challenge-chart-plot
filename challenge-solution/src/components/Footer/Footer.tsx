21ca961f6fa89cc09a7c0af4455a23802dfc3c35
Khaled <khalegjr@gmail.com>
2023-01-09 05:14:07 -0300

    Implement styling in the Footer component

--- challenge-solution/src/components/Footer/Footer.tsx
+++ challenge-solution/src/components/Footer/Footer.tsx
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+
+// Create a Wrapper component that'll render a <header> tag with some styles
+const Wrapper = styled.footer`
+  padding: 0.5em 1em;
+  background: #dddee1;
+  width: 100%;
+`;
+
+const ButtonChart = styled.button`
+  padding: 0.5em 1em;
+  margin: 1em 0;
+  background: #017eff;
+  color: #fff;
+  border-radius: 5px;
+`;
+
 const Footer = () => {
-  return <button>generate chart</button>;
+  return (
+    <Wrapper style={{ position: "absolute", bottom: 0 }}>
+      <ButtonChart>GENERATE CHART</ButtonChart>
+    </Wrapper>
+  );
 };
 
 export default Footer;
